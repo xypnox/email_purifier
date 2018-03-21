8d72dd7a1b7dc52593be159d1e98c108001d2953
Aditya Vikram Singh <xypnox@gmail.com>
2018-03-21 21:37:06 +0530

    Indented and cleaned up code

--- email_checker.py
+++ email_checker.py
@@ -2,34 +2,41 @@ valid_email = ['gmail.com', 'yahoo.co.in', 'iitkgp.ac.in']
 
 
 def perms(word):
-    "All edits that are one edit away from `word`."
+    '''All edits that are one edit away from `word`.'''
     letters = 'qwertyuiopasdfghjklzxcvbnm'
+
     splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
     deletes = [L + R[1:] for L, R in splits if R]
     transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
     replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
     inserts = [L + c + R for L, R in splits for c in letters]
-    return deletes + transposes + replaces + inserts
+
+    return set(deletes + transposes + replaces + inserts)
 
 
 class EmailPurifier:
-    def __init__(self, emails):
+    def __init__(self, emails, check=False):
         self.emails = emails
-        return self.CheckEmail()
+        if check is True:
+            self.CheckEmail()
 
-    def CheckEmail(self, checkTypo=False):
-        correct = 0
+    def CheckEmail(self, checkTypo=False, echoText=False):
+        self.correct = 0
         for email in self.emails:
             contents = email.split('@')
             if len(contents) == 2:
+
                 if contents[1] in valid_email:
-                    correct += 1
+                    self.correct += 1
                     print('It is correct')
+
                 elif checkTypo is True:
                     domain_data = contents[1].split('.')
                     for vemail in valid_email:
                         alters = perms(vemail.split('.', 1)[0])
-                        if domain_data[0] in alters:
-                            print("Wrong Domain : " + contents[1])
-                            print("Did you mean : " + vemail)
-                            break
+
+                        if domain_data[0] in alters and echoText is True:
+                                print("Wrong Domain : " + contents[1])
+                                print("Did you mean : " + vemail)
+                                break
+        return self.correct
--- tezt.py
+++ tezt.py
@@ -2,14 +2,20 @@ def edits1(word):
     "All edits that are one edit away from `word`."
     letters = 'qwertyuiopasdfghjklzxcvbnm'
     splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    print('splits = ', splits)
     deletes = [L + R[1:] for L, R in splits if R]
+    print('deletes = ', deletes)
     transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+    print('transposes = ', transposes)
     replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
+    print('replaces = ', replaces)
     inserts = [L + c + R for L, R in splits for c in letters]
+    print('inserts = ', inserts)
+    print(deletes + transposes + replaces + inserts)
+    print(len(set(deletes + transposes + replaces + inserts)))
     return deletes + transposes + replaces + inserts
 
 
-if 'gamil' in edits1('gmail'):
-    print("Hell Yeah")
+edits1('gmail')
 
 print('gmail.co.in'.split('.', 1)[0])
